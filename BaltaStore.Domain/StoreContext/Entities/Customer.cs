74578d17bf6666cae715b2d8ed92dceaa2b9c50d
Wiliam Paulino <willjcpower@gmail.com>
2020-09-18 13:14:28 -0300

    finalizacao do modelo

--- BaltaStore.Domain/StoreContext/Entities/Customer.cs
+++ BaltaStore.Domain/StoreContext/Entities/Customer.cs
@@ -1,33 +1,40 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using BaltaStore.Domain.StoreContext.ValueObjects;
 
 namespace BaltaStore.Domain.StoreContext.Entities
 {
     public class Customer
     {
-        public Customer(string firstName,
-            string lastName,
-            string document,
-            string email,
-            string phone,
-            string address)
+        private readonly IList<Address> _addresses;
+        public Customer(Name name,
+            Document document,
+            Email email,
+            string phone)
         {
-            FirstName = firstName;
-            LastName = lastName;
+            Name = name;
             Document = document;
             Email = email;
             Phone = phone;
-            Address = address;
+            _addresses = new List<Address>();
         }
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public string Document { get; private set; }
-        public string Email { get; private set; }
+        public Name Name { get; private set; }
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public string Address { get; private set; }
+        public IReadOnlyCollection<Address> Addresses => _addresses.ToArray();
+
+        public void AddAddress(Address address)
+        {
+            //Validar o Endereço
+            //Adicionar o Endereço
+            _addresses.Add(address);
+        }
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName}";
+            return Name.ToString();
         }
     }
 }
